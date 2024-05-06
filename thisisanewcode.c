c7bf81c97c1de1645506b653aeeb4b82d7ca994e
sarhansaad2004 <sarhansaad2004@gmail.com>
2024-05-07 00:20:48 +0600

    Index new code added

--- thisisanewcode.c
+++ thisisanewcode.c
@@ -1,6 +1,10 @@
 #include<stdio.h>
 int main()
 {
-    printf("HEllo World");
+int sum=0,a,b;
+printf("Enter 2 numbers = ");
+scanf("%d%d",&a,&b);
+sum=a+b;
+printf("The sum is = %d",sum);
     return 0;
 }
